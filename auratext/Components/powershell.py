c68d82b06e24a82ad06ecdda8b1cb5a297fa8bf7
Matthew Yang <matthew.yang204@gmail.com>
2026-04-14 19:53:05 -0700

    Powershell already has a prompt, we do not need one.

--- auratext/Components/powershell.py
+++ auratext/Components/powershell.py
@@ -94,7 +94,7 @@ class TerminalEmulator(QWidget):
         self.history_index = 0
 
         self.current_command = ""
-        self.prompt = "> "
+        self.prompt = ""
 
         self.addNewTab()
 
@@ -196,7 +196,7 @@ class TerminalEmulator(QWidget):
     def switchTab(self, index):
         self.current_process_index = index
         self.terminal.clear()
-        self.terminal.appendPlainText("> ")
+        # self.terminal.appendPlainText("")
 
     def closeTab(self, index):
         if self.tabBar.count() > 1:
@@ -238,8 +238,8 @@ class TerminalEmulator(QWidget):
         self.terminal.moveCursor(QTextCursor.MoveOperation.End)
         self.insert_colored_text(data)
         self.terminal.moveCursor(QTextCursor.MoveOperation.End)
-        if not data.endswith("\n"):
-            self.terminal.insertPlainText("\n")
+        # if not data.endswith("\n"):
+        #     self.terminal.insertPlainText("\n")
         self.display_prompt()
 
     def handle_stderr(self):
@@ -257,7 +257,7 @@ class TerminalEmulator(QWidget):
         self.display_prompt()
 
     def display_prompt(self):
-        self.terminal.appendPlainText(self.prompt)
+        # self.terminal.appendPlainText(self.prompt)
         self.terminal.moveCursor(QTextCursor.MoveOperation.End)
 
     def insert_colored_text(self, text, default_color=QColor(255, 255, 255)):
@@ -326,7 +326,7 @@ class TerminalEmulator(QWidget):
                 QPlainTextEdit.keyPressEvent(self.terminal, event)
 
     def execute_command(self):
-        self.terminal.appendPlainText("")
+        # self.terminal.appendPlainText("")
         self.processes[self.current_process_index].write(
             self.current_command.encode() + b"\n"
         )
@@ -363,7 +363,7 @@ class TerminalEmulator(QWidget):
     def run_command(self, command):
         self.terminal.moveCursor(QTextCursor.MoveOperation.End)
         self.terminal.insertPlainText(f"{self.prompt}{command}\n")
-        self.processes[self.current_process_index].write(command.encode() + b"\n")
+        self.processes[self.current_process_index].write(command.encode())
 
     def run_file(self, file_path):
         file_name = os.path.basename(file_path)
