98646315873e899eb6b181f57aaad8f404da927c
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-04-24 17:09:09 -0700

    Enable Aura Text to refresh and update its own appdata with any missing files it may have in its appdata

--- auratext/Misc/quirks.py
+++ auratext/Misc/quirks.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 def get_linux_productname():
     print("Getting Linux product name in order to apply quirks...", end='')
@@ -10,3 +11,10 @@ def get_linux_productname():
 def crosvm_quirks():
     print("Applying Chrome OS crostini quirks...")
     os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+def copy_if_not_exists(src, dst, *, follow_symlinks=True):
+    if not os.path.exists(dst):
+        print(f"{dst} does not exist, copying...")
+        return shutil.copy2(src, dst, follow_symlinks=follow_symlinks)
+    else:
+        print(f"{dst} already exists, not copying...")
