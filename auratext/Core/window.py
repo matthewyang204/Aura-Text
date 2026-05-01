4b41abaeb18e72cff9dff0c682d5ccf31bcccb35
Matthew Yang <matthew.yang204@gmail.com>
2026-05-01 15:58:56 -0700

    Fix duplicate imports (massive performance penalty)

--- auratext/Core/window.py
+++ auratext/Core/window.py
@@ -48,7 +48,6 @@ from . import ThemeDownload
 from . import config_page
 from .CommandPalette import CommandPalette
 from ..Components import powershell, terminal, statusBar, ProjectManager, About, ToDo, GitGraph
-from ..Components import powershell, terminal, statusBar, ProjectManager, About, ToDo, GitGraph, GitRebase, Performance, DBViewer
 from ..Components.CommandPalette import CommandPalette
 from ..Components.NewProjectDialog import NewProjectDialog
 from ..Components.Linter import CodeLinter
