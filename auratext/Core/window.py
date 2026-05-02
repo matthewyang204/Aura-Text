3d9731a5ef7fb9ac9ad5ba502ff09b88faf021ca
Matthew Yang (杨佳明) <matthew.yang204@gmail.com>
2026-05-02 07:56:53 -0700

    Use get_appdata_dirs in window.py

--- auratext/Core/window.py
+++ auratext/Core/window.py
@@ -62,21 +62,9 @@ from .AuraText import CodeEditor
 from auratext.Components.TabWidget import TabWidget
 from .plugin_interface import Plugin
 from notepadequalequal.fileio import retrieve_file
-from auratext.Misc.boilerplates import get_font_for_platform, pathspec_gitignore_parse, is_under_parent_list
+from auratext.Misc.boilerplates import get_font_for_platform, pathspec_gitignore_parse, is_under_parent_list, get_appdata_dirs
 
-if platform.system() == "Windows":
-    local_app_data = os.getenv('LOCALAPPDATA')
-elif platform.system() == "Linux":
-    local_app_data = os.path.expanduser("~/.config")
-elif platform.system() == "Darwin":
-    local_app_data = os.path.expanduser("~/Library/Application Support")
-else:
-    print("Unsupported operating system")
-    sys.exit(1)
-
-local_app_data = os.path.join(local_app_data, "AuraText")
-
-script_dir = os.path.dirname(os.path.abspath(__file__))
+local_app_data, script_dir = get_appdata_dirs()
 
 try:
     cpath = retrieve_file(f"{local_app_data}/data/CPath_Project.txt").strip()
